fa798d4770d72848afbd4791ddd5ffb7ec4890b9
yanqiu shen <yanqiu.shen@hand-china.com>
2018-09-24 22:49:38 +0800

    fix ss

--- src/main.js
+++ src/main.js
@@ -1,15 +1,1 @@
-import $ from 'jquery'
-
-import './css/index.css'
-import './css/index.less'
-import './css/index.scss'
-import 'bootstrap/dist/css/bootstrap.min.css'
-
-$(function(){
-  $("ul li:odd").css("background-color", 'lightgreen')
-  $("ul li:even").css("background-color", 'lightyellow')
-})
-
-class Person {
-  static info = { name: 'syq', age: 20 }
-}
+console.log("ok")
