bc5067c602918ac4c2c5c2647251823544a0b197
18305 <yanqiu.shen@hand-china.com>
2018-09-25 22:59:46 +0800

    webpack-mintui

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>这是APP组件</h1>
+    <mt-button type="default" @click="showT">default</mt-button>
     <router-link to="/account">account</router-link>
     <router-link to="/goodslist">goodslist</router-link>
     <router-view/>
@@ -8,15 +9,29 @@
 </template>
 
 <script>
+import { Toast } from "mint-ui";
 export default {
-  name: 'app',
+  name: "app",
   data: () => {
-    return {}
+    return {};
+  },
+  methods: {
+    showT() {
+      // Toast('提示信息');
+      // Toast({
+      //   message: "提示",
+      //   position: "bottom",
+      //   duration: 5000
+      // });
+      Toast({
+        message: "操作成功",
+        iconClass: "icon icon-success"
+      });
+    }
   }
-}
+};
 </script>
 
 <style>
-
 </style>
 
--- src/main.js
+++ src/main.js
@@ -2,6 +2,17 @@ import Vue from 'vue'
 import app from './App'
 import router from './router'
 
+// 全部引入
+// import MintUI from 'mint-ui'
+// import 'mint-ui/lib/style.css'
+
+// Vue.use(MintUI)
+
+// 按需引入
+import { Button, Cell } from 'mint-ui'
+Vue.component(Button.name, Button)
+Vue.component(Cell.name, Cell)
+
 new Vue({
   el: '#app',
   router,
